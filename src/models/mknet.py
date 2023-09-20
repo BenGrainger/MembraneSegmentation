385e052542f3aa5ab2a0b7faf0812784e43d838a
BenGrainger <bfgrainger@gmail.com>
2023-09-20 15:33:10 +0100

    added preprocess

---
+++ src/models/mknet.py
@@ -0,0 +1,117 @@
+import torch
+from unet import UNet, ConvPass
+
+def create_affinity_model(num_fmaps, fmap_inc_factor, downsample_factors):
+    """
+    Args:
+
+            num_fmaps:
+
+                The number of feature maps in the first layer. This is also the
+                number of output feature maps of the Unet before passing to the final convolutional layer. Stored in the ``channels``
+                dimension. this is often 12
+
+            fmap_inc_factor:
+
+                By how much to multiply the number of feature maps between
+                layers. If layer 0 has ``k`` feature maps, layer ``l`` will
+                have ``k*fmap_inc_factor**l``. this is often 6
+
+            downsample_factors:
+
+                List of tuples ``(z, y, x)`` to use to down- and up-sample the
+                feature maps between layers.
+
+    """
+
+    # create unet
+    unet = UNet(
+        in_channels=1,
+        num_fmaps=num_fmaps,
+        fmap_inc_factor=fmap_inc_factor,
+        downsample_factors=downsample_factors)
+
+    # add an extra convolution to get from 12 feature maps to 3 (affs in x,y,z)
+    model = torch.nn.Sequential(
+        unet,
+        ConvPass(in_channels=num_fmaps, out_channels=3, kernel_sizes=[[1, 1, 1]], activation='Sigmoid'))
+    
+    return model
+
+
+
+
+def create_lsd_model(num_fmaps, fmap_inc_factor, downsample_factors):
+    """
+    Args:
+
+            num_fmaps:
+
+                The number of feature maps in the first layer. This is also the
+                number of output feature maps of the Unet before passing to the final convolutional layer. Stored in the ``channels``
+                dimension. this is often 12
+
+            fmap_inc_factor:
+
+                By how much to multiply the number of feature maps between
+                layers. If layer 0 has ``k`` feature maps, layer ``l`` will
+                have ``k*fmap_inc_factor**l``. this is often 6
+
+            downsample_factors:
+
+                List of tuples ``(z, y, x)`` to use to down- and up-sample the
+                feature maps between layers.
+
+    """
+
+    # create unet
+    unet = UNet(
+        in_channels=1,
+        num_fmaps=num_fmaps,
+        fmap_inc_factor=fmap_inc_factor,
+        downsample_factors=downsample_factors)
+
+    model = torch.nn.Sequential(
+        unet,
+        ConvPass(in_channels=num_fmaps, out_channels=10, kernel_sizes=[[1, 1, 1]], activation='Sigmoid'))
+    
+    return model
+
+
+
+
+def create_aclsd_model(num_fmaps, fmap_inc_factor, downsample_factors):
+    """
+    Args:
+
+            num_fmaps:
+
+                The number of feature maps in the first layer. This is also the
+                number of output feature maps of the Unet before passing to the final convolutional layer. Stored in the ``channels``
+                dimension. this is often 12
+
+            fmap_inc_factor:
+
+                By how much to multiply the number of feature maps between
+                layers. If layer 0 has ``k`` feature maps, layer ``l`` will
+                have ``k*fmap_inc_factor**l``. this is often 6
+
+            downsample_factors:
+
+                List of tuples ``(z, y, x)`` to use to down- and up-sample the
+                feature maps between layers.
+
+    """
+
+    # create unet
+    unet = UNet(
+        in_channels=10,
+        num_fmaps=num_fmaps,
+        fmap_inc_factor=fmap_inc_factor,
+        downsample_factors=downsample_factors)
+
+    model = torch.nn.Sequential(
+        unet,
+        ConvPass(in_channels=num_fmaps, out_channels=3, kernel_sizes=[[1, 1, 1]], activation='Sigmoid'))
+
+    return model
---
+++ src/pre/preprocess.py
@@ -0,0 +1,79 @@
+import gunpowder as gp
+from add_local_shape_descriptor import AddLocalShapeDescriptor
+import math
+
+
+def initialize_pipeline(sources, raw, labels):
+
+    pipeline = sources
+
+    # randomly choose a sample from our tuple of samples - this is absolutely necessary to be able to get any data!
+    pipeline += gp.RandomProvider()
+
+    # add steps to the pipeline
+    #randomly mirror and transpose a batch
+    pipeline == gp.SimpleAugment()
+
+    # elastcally deform the batch
+    pipeline += gp.ElasticAugment(
+        [4,40,40],
+        [0,2,2],
+        [0,math.pi/2.0],
+        prob_slip=0.05,
+        prob_shift=0.05,
+        max_misalign=25)
+
+    # randomly shift and scale the intensities
+    pipeline += gp.IntensityAugment(
+        raw,
+        scale_min=0.9,
+        scale_max=1.1,
+        shift_min=-0.1,
+        shift_max=0.1,
+        z_section_wise=True)
+    
+    # dilate the boundaries between labels
+    pipeline += gp.GrowBoundary(labels, 
+                             steps=3,
+                             only_xy=True)
+    
+    return pipeline
+
+
+def add_affinity_pipeline(pipeline, labels, gt_affs, affs_weights):
+
+    pipeline += gp.AddAffinities(
+        affinity_neighborhood=[[-1, 0, 0], [0, -1, 0], [0, 0, -1]],
+        labels=labels,
+        affinities=gt_affs)
+
+    # no longer need labels since we use the gt affs for training
+
+    # create scale array to balance class losses (will then use the affs_weights array during training)
+    pipeline += gp.BalanceLabels(
+            gt_affs,
+            affs_weights)
+    
+def add_lsd_pipeline(pipeline, labels, gt_lsds, lsds_weights):
+
+    pipeline += AddLocalShapeDescriptor(
+        labels,
+        gt_lsds,
+        lsds_mask=lsds_weights,
+        sigma=80,
+        downsample=2)
+
+
+def create_lsd_pipeline(sources, raw, labels, gt_lsds, lsds_weights):
+    pipeline = initialize_pipeline(sources, raw, labels)
+    add_lsd_pipeline(pipeline, labels, gt_lsds, lsds_weights)
+    pipeline += gp.Unsqueeze([raw])
+    pipeline += gp.Stack(1)
+
+    
+
+def create_aff_pipeline(sources, raw, labels, gt_affs, affs_weights):
+    pipeline = initialize_pipeline(sources, raw, labels)
+    add_affinity_pipeline(pipeline, labels, gt_affs, affs_weights)
+    pipeline += gp.Unsqueeze([raw])
+    pipeline += gp.Stack(1)
