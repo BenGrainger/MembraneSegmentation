4fa5bd0bdf95711cfd7e677fdfe37899dc76191f
BenGrainger <bfgrainger@gmail.com>
2023-09-25 15:05:11 +0100

    push

--- scripts/train_LSD.py
+++ scripts/train_LSD.py
@@ -1,4 +1,3 @@
-#%%
 import gunpowder as gp
 
 from MembraneSegmentation.io.dataloaders import dataloader_zarrmultiplesources3D
@@ -8,17 +7,15 @@ from MembraneSegmentation.post.train import train
 from MembraneSegmentation.utils.script_setup import ScriptSetup, check_folder_exists
 
 print('loading config')
-config_path = r'config_files/LSD_config.json'
+config_path = r'config/LSD/LSD_config.json'
 
 script = ScriptSetup(config_path)
 script.load_script()
-config = script.return_config
-logger = script.return_logger
-out_dir = script.return_out_dir
+config = script.return_config()
+logger = script.return_logger()
+out_dir = script.return_out_dir()
 
-#%%
-
-print('establishing parameters')
+logging.info('establishing parameters')
 parent_dir = config['parent_dir'] 
 data_dir_list = config["data_dir_list"]
 data_dir_list = [i for i in data_dir_list.values()]
@@ -53,22 +50,22 @@ check_folder_exists(log_dir)
 
 batch_dict = {'RAW': raw, 'LABELS': labels, 'GT_LSDS': gt_lsds, 'LSDS_WEIGHTS': lsds_weights, 'PRED_LSDS': pred_lsds}
 
-print('creating data source')
+logging.info('creating data source')
 sources  = dataloader_zarrmultiplesources3D(raw, labels, parent_dir, data_dir_list)
 
-print('creating data pipeline')
+logging.info('creating data pipeline')
 pipeline = preprocessing_pipeline(sources, raw, labels, None)
 pipeline.create_pipeline()
 pipeline.add_lsd_pipeline(gt_lsds, lsds_weights)
 pipeline.add_final_prepprocess_pipeline()
 
-print('creating model')
+logging.info('creating model')
 lsd_model = mknet(num_fmaps, fmap_inc_factor, downsample_factors, model=None)
 lsd_model.create_LSD_model()
 input_size, output_size = lsd_model.return_input_output_sizes(input_shape, voxel_size)
 lsd_model = lsd_model.get_model()
 
-print('request batch')
+logging.info('request batch')
 request = gp.BatchRequest()
 request.add(raw, input_size)
 request.add(labels, output_size)
@@ -76,14 +73,14 @@ request.add(gt_lsds, output_size)
 request.add(lsds_weights, output_size)
 request.add(pred_lsds, output_size)
 
-print('load model into pipeline')
+logging.info('load model into pipeline')
 outputs = [pred_lsds]
 loss_inputs = [pred_lsds, gt_lsds, lsds_weights]
 pipeline.add_model(lsd_model, raw, outputs, loss_inputs, checkpoint_basename, log_dir, save_every=1, log_every=1)
 
 pipeline = pipeline.get_pipeline()
 
-print('train model')
+logging.info('train model')
 train(request, pipeline, batch_dict, voxel_size).gunpowder_train(max_iteration=30000, test_training=False)
 
 
---
+++ scripts/train_LSD.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+#SBATCH -p gpu # partition (queue)
+#SBATCH --exclude=gpu-sr670-20
+#SBATCH -N 1   # number of nodes
+#SBATCH --mem 120G # memory pool for all cores
+#SBATCH --gres gpu:rtx5000:1
+#SBATCH -t 15-0:0 # time (D-HH:MM)
+#SBATCH -o lsdtrain.out
+#SBATCH -e lsdtrain.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ucqfbfg@ucl.ac.uk
+
+
+echo "loading minconda"
+module load miniconda/4.9.2
+echo "loading cuda"
+module load cuda/12.0
+echo "initializing environment"
+conda activate gunpowder
+echo "starting script"
+python3 train_LSD.py
